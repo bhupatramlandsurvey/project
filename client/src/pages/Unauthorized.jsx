dab046f5fc095a0a77da07fcea53a2ca45c59f3e
DineshJuluri <dineshkumar9100572140@gmail.com>
2026-01-29 21:00:44 +0530

    final update

--- client/src/pages/Unauthorized.jsx
+++ client/src/pages/Unauthorized.jsx
@@ -1,7 +1,7 @@
 export default function Unauthorized() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-3xl font-bold text-red-600 mb-4">Access Denied 🚫</h1>
+      <h1 className="text-3xl font-bold text-red-600 mb-4">Access Denied 🚫🚫🚫</h1>
       <p className="text-gray-600">You don’t have permission to view this page.</p>
     </div>
   );
